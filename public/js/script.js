165c339a67824679b5776ed8896561c1b68d934b
ivan-roon <ivan@roonlabs.com>
2020-09-22 12:47:59 +0300

    Setup app for heroku

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
 	const location = searchElement.value;
 	messageTwo.textContent = 'Loading';
 	messageOne.textContent = '';
-	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+	fetch('/weather?address=' + location).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				messageTwo.textContent = data.error;
